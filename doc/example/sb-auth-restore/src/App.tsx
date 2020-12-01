8f0c1bdae52a292f5a8d2221766d00932d3598b3
Shorn <github@shorn.kopi.cloud>
2020-12-01 13:11:11 +1000

    add commit_ref

--- doc/example/sb-auth-restore/src/App.tsx
+++ doc/example/sb-auth-restore/src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+const reactGitCommit = process.env.REACT_APP_COMMIT_REF;
+const directGitCommit = process.env.COMMIT_REF;
+
 export function App(){
   return <div>
-    sb-auth-restore example project
+    <div>sb-auth-restore example project</div>
+    <div>Build commit: {reactGitCommit}</div>
+    <div>Build commit: {directGitCommit}</div>
+    <div></div>
+
   </div>
 }
 
