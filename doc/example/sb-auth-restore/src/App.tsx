cde2fd3e56cc86af97ac051c039aed6b33ac35ed
Shorn <github@shorn.kopi.cloud>
2020-12-01 13:22:02 +1000

    cleanup example

--- doc/example/sb-auth-restore/src/App.tsx
+++ doc/example/sb-auth-restore/src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
+// supplied by the build command in netlify.toml
 const reactGitCommit = process.env.REACT_APP_COMMIT_REF;
-const directGitCommit = process.env.COMMIT_REF;
 
 export function App(){
+  return <>
+    <Info/>
+  </>
+}
+
+function Info(){
   return <div>
     <div>sb-auth-restore example project</div>
-    <div>Build commit: {reactGitCommit}</div>
-    <div>Build commit: {directGitCommit}</div>
-    <div></div>
-
+    <div>Built from Git commit: {reactGitCommit}</div>
   </div>
 }
 
