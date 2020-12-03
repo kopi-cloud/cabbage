52faa9d06fbecf9f54b4345effed09e0a8e68524
Shorn <github@shorn.kopi.cloud>
2020-12-03 17:09:27 +1000

    fix oauth redirect to not flicker

--- app/src/Api/SupabaseProvider.tsx
+++ app/src/Api/SupabaseProvider.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
-import {createContext, ReactNode, useContext, useEffect, useState} from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useRef,
+  useState
+} from "react";
 import {SmallScreenSpinner} from "Component/SmallScreenSpinner";
 import {Config} from "Config";
 import {createClient} from "@supabase/supabase-js";
-import { TextSpan } from "Component/TextSpan";
+import {TextSpan} from "Component/TextSpan";
 import {SmallScreenContainer} from "Component/Screen";
 import SupabaseClient from "@supabase/supabase-js/dist/main/SupabaseClient";
 import {
@@ -11,8 +18,7 @@ import {
   Session,
   User
 } from '@supabase/gotrue-js/dist/main/lib/types';
-import { STORAGE_KEY } from '@supabase/gotrue-js/dist/main/lib/constants';
-import {safeStringify} from "Util/ObjectUtil";
+import {STORAGE_KEY} from '@supabase/gotrue-js/dist/main/lib/constants';
 
 const log = console;
 
@@ -31,6 +37,7 @@ export const useSupabase = ()=> useContext(SupabaseApiContext);
 export function SupabaseProvider({children}: {children: ReactNode}){
   const [apiState, setApiState] = useState(undefined as undefined|SupabaseApi);
   const [isAnonKeyValid, setIsAnonKeyValid] = useState(true);
+  const isOauthRedirect = useRef(false);
 
   /* IMPROVE: too verbose both in terms of code and logging.
    Most conditionals can probably be collapsed if the logging is removed,
@@ -48,6 +55,10 @@ export function SupabaseProvider({children}: {children: ReactNode}){
     ){
       log.debug("supabase auth state change", {
         authEvent, session, user: session?.user });
+      if( isOauthRedirect.current && session ){
+        log.debug("session restored from oauth redirect");
+        isOauthRedirect.current = false;
+      }
       setApiState((apiState)=>{
         if( !apiState ) throw new Error("change event with no apiState");
         return {db: apiState.db, session: session, user: session?.user ?? null}
@@ -55,6 +66,12 @@ export function SupabaseProvider({children}: {children: ReactNode}){
     }
 
     const newClient = createClient(Config.supabaseUrl, Config.supabaseAnonKey)
+
+    if( window.location.hash.indexOf('access_token') >= 0 ){
+      log.debug("detected this is an oauth redirect");
+      isOauthRedirect.current = true;
+    }
+
     const subscription = newClient.auth.onAuthStateChange(onAuthStateChange);
     function cleanup(){
       subscription.data?.unsubscribe();
@@ -74,13 +91,12 @@ export function SupabaseProvider({children}: {children: ReactNode}){
     if( !localStorage.getItem(STORAGE_KEY) ){
       /* user never logged in, deleted localstorage, previously logged out,
       or we're being landing a redirect for google SSO */
-      log.debug("no supabase token found in localstorage",
-        safeStringify(newClient));
-      setApiState({
-        db: newClient,
-        session: newClient.auth.session(),
-        user: newClient.auth?.session()?.user ?? null });
-      return cleanup;
+      log.debug("X no supabase token found in localstorage");
+      // setApiState({
+      //   db: newClient,
+      //   session: newClient.auth.session(),
+      //   user: newClient.auth?.session()?.user ?? null });
+      // return cleanup;
     }
 
     /* session restore is async, see:
@@ -123,6 +139,11 @@ export function SupabaseProvider({children}: {children: ReactNode}){
   if( !apiState ){
     log.debug("showing spinner while creating supabase apiState");
     return <SmallScreenSpinner message={"Configuring Supabase API"}/>
+  }
+
+  if( isOauthRedirect.current ){
+    log.debug("showing spinner while waiting for oauth login");
+    return <SmallScreenSpinner message={"Processing SSO identity"}/>
   }
 
   return <SupabaseApiContext.Provider value={apiState}>
