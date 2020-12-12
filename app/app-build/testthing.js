ff5f6e77c19d4cb2d06c72dd3c53dce362743ce9
Shorn <216456+shorn@users.noreply.github.com>
2020-12-12 14:13:19 +1000

    start the app-build idea

---
+++ app/app-build/testthing.js
@@ -0,0 +1,5 @@
+/**
+ * I'm planning that this is where I will write my own code to generate
+ * types into `/src/Generated` since openapi-typescript is not very useful.
+ */
+console.log("from testthing");
