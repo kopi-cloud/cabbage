7d2e00ab90753bddf25b26577a76bfff4002921c
Shorn <216456+shorn@users.noreply.github.com>
2020-12-13 15:04:02 +1000

    implement display_name hack test

--- app/src/Screen/ScratchScreen.tsx
+++ app/src/Screen/ScratchScreen.tsx
@@ -1,6 +1,5 @@
-import {Config} from "Config";
 import {NavTransition} from "Navigation/NavigationProvider";
-import React, {useState} from "react";
+import React, {ChangeEvent, SyntheticEvent, useCallback, useState} from "react";
 import {SmallScreenContainer} from "Component/Screen";
 import {TextSpan} from "Component/TextSpan";
 import {SecondaryButton} from "Component/CabbageButton";
@@ -8,22 +7,24 @@ import {useSupabase} from "Api/SupabaseProvider";
 import {stopClick} from "Util/EventUtil";
 import {ButtonContainer} from "Component/ButtonContainer";
 import {ErrorInfo} from "Error/ErrorUtil";
+import Typography from "@material-ui/core/Typography";
+import {TextField} from "@material-ui/core";
+import {useAuthnUser} from "Api/AuthenticatedUserProvider";
+import {private_user_info, public_user_info, Tables} from "Api/CabbageSchema";
+import {CurrentUser} from "Component/CurrentUser";
 
 const log = console;
 
-const otherUrl = "/scratch";
+const scratchUrl = "/scratch";
 
 export function getScratchScreenLink(): string{
-  return otherUrl;
+  return scratchUrl;
 }
 
 export function isScratchScreenPath(path: String): boolean{
   const normalizedPath = path.toLowerCase();
-  return normalizedPath.startsWith(otherUrl);
+  return normalizedPath.startsWith(scratchUrl);
 }
-
-const swaggerUrl =
-  `${Config.supabaseUrl}/rest/v1/?apikey=${Config.supabaseAnonKey}`;
 
 export function ScratchScreen(){
   return <NavTransition isPath={isScratchScreenPath} title={"Cabbage - scratch"}>
@@ -37,6 +38,7 @@ function Content(){
     <ButtonContainer>
       <ApiRestartButton/>
     </ButtonContainer>
+    <HackDisplayNameContainer stompValue={'xxx'}/>
   </SmallScreenContainer>
 }
 
@@ -61,4 +63,82 @@ function ApiRestartButton(){
         setIsRestarting(false);
       }}
     >API restart</SecondaryButton>
+}
+
+export function HackDisplayNameContainer({stompValue}:{
+  stompValue: string,
+}){
+  const [uuid, setUuid] = useState("");
+  const [isWriting, setIsWriting] = useState(false);
+  const [writeError, setWriteError] =
+    useState(undefined as undefined | ErrorInfo);
+  const {db} = useAuthnUser();
+
+  const onUuidChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUuid(event.currentTarget.value as string);
+  };
+
+  const onHackDisplayNameClick = useCallback(async(e: SyntheticEvent)=>{
+    stopClick(e);
+    setIsWriting(true);
+    setWriteError(undefined);
+
+    const result = await db.from<public_user_info>(Tables.public_user_info).
+      insert({uuid, display_name: stompValue}, {upsert: true});
+    log.debug("hack result", {myId: db.auth.user()?.id, result});
+    if( result.error ){
+      setWriteError({message: result.error.message, problem: result.error});
+    }
+    setIsWriting(false);
+  }, [db, uuid, stompValue]);
+
+  const onHackContactDetailsClick = useCallback(async(e: SyntheticEvent)=>{
+    stopClick(e);
+    setIsWriting(true);
+    setWriteError(undefined);
+
+    const result = await db.from<private_user_info>(Tables.private_user_info).
+      insert({uuid, contact_details: stompValue}, {upsert: true});
+    log.debug("hack result", {myId: db.auth.user()?.id, result});
+    if( result.error ){
+      setWriteError({message: result.error.message, problem: result.error});
+    }
+    setIsWriting(false);
+  }, [db, uuid, stompValue]);
+
+  return <>
+    <Typography variant={"h5"} style={{textAlign: "center"}}>
+      Stomp someone else's display name
+    </Typography>
+    <div>
+      <CurrentUser/>
+      <Typography>Your own UUID is {db.auth.user()?.id}</Typography>
+    </div>
+    <form autoComplete="on" noValidate>
+      <TextField id="uuidInputField" label="UUID"
+        value={uuid}
+        onChange={onUuidChange}
+        disabled={isWriting}
+        margin="normal"
+        variant="outlined"
+        autoComplete="on"
+        fullWidth={true}
+        inputProps={{autoCapitalize: "none"}}
+      />
+      <ButtonContainer style={{justifyContent: 'center', marginTop: "1em"}}
+        error={writeError}
+      >
+        <SecondaryButton type="button" isLoading={isWriting}
+          disabled={isWriting} onClick={onHackDisplayNameClick}
+        >
+          Set display_name = '{stompValue}'
+        </SecondaryButton>
+        <SecondaryButton type="button" isLoading={isWriting}
+          disabled={isWriting} onClick={onHackContactDetailsClick}
+        >
+          Set contact_details = '{stompValue}'
+        </SecondaryButton>
+      </ButtonContainer>
+    </form>
+  </>
 }
