69b2bfbcc63ea5184b16d3580228a8311ec8bc24
Shorn <216456+shorn@users.noreply.github.com>
2021-02-28 10:15:31 +1000

    factor out CommitInfo.tsx
    fix path tsconfig setting

--- doc/example/pull-routing/src/App.tsx
+++ doc/example/pull-routing/src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-
-// supplied by the build command in netlify.toml
-const gitCommit = process.env.REACT_APP_COMMIT_REF;
+import {CommitInfo} from "CommitInfo";
 
 // const log = console;
 
 export function App(){
   return <>
-    <Info/>
+    <CommitInfo/>
   </>
-}
-
-function Info(){
-  const sourceUrl = `https://github.com/kopi-cloud/cabbage/tree/${gitCommit}` +
-    '/doc/example/pull-routing';
-  return <div>
-    <div>eg-pull-routing example project</div>
-    <div>
-      Source on <a href={sourceUrl}>Github</a>,
-      built from commit: {gitCommit}
-    </div>
-  </div>
 }
 
