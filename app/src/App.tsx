3596262156bbf95956a767dcfda92a1ea69af6c4
Shorn <bitbucket-shorn@snkmail.com>
2021-12-23 13:27:03 +1000

    layout imports

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.css';
+
 import {CssBaseline} from "@mui/material";
-import {ErrorDialogProvider} from "Error/ErrorDialog";
+import {CabbageTheme} from "Component/CabbageTheme";
 import {ReactErrorBoundary} from "Error/ReactErrorBoundary";
-import {SupabaseProvider} from "Api/SupabaseProvider";
+import {ErrorDialogProvider} from "Error/ErrorDialog";
 import {NavigationProvider} from "Navigation/NavigationProvider";
-import {UserEditScreen} from "Screen/User/UserEditScreen";
+import {SupabaseProvider} from "Api/SupabaseProvider";
 import {AuthenticatedUserProvider} from "Api/AuthenticatedUserProvider";
-import {OtherScreen} from "Screen/OtherScreen";
+
 import {AppNavBar} from "Navigation/AppNavBar";
+import {UserEditScreen} from "Screen/User/UserEditScreen";
+import {OtherScreen} from "Screen/OtherScreen";
 import {ScratchScreen} from "Screen/ScratchScreen";
 import {ErrorHandlingScreen} from "Screen/ErrorHandling";
 import {DatabaseSchemaScreen} from "Screen/DatabaseSchemaScreen";
 import {UserListScreen} from "Screen/User/UserListScreen";
 import {UserDisplayScreen} from "Screen/User/UserDisplayScreen";
-import {CabbageTheme} from "Component/CabbageTheme";
 
 export function App(){
   return <CabbageTheme>
